0512501d9fcbf3e739b0ef53ee93778804b4d4f2
Nubunduram <ceantoine@hotmail.fr>
2024-11-07 15:26:40 +0100

    exo cercle de couleur

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-// Mettre en place un tableau contenant un ensemble de couleur
 const colors = ['#FF5733', '#33FF57', '#3357FF', '#F9E800', '#8E44AD', '#1ABC9C'];
 
 document.addEventListener('click', function (event) {
 
-    // On demande la position verticale et horizontal du click dans la page (event.pageX, event.pageY)
+    // Récupere la position du click
     const posX = event.pageX;
     const posY = event.pageY;
-
+    
     // Générer une taille aléatoire entre 50 et 150 pixels
     const size = Math.floor(Math.random() * 101) + 50;
 
     // Créer le cercle 
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('circle');
-    newDiv.style.width = `${size}px`;
-    newDiv.style.height = `${size}px`;
-    newDiv.style.left = `${posX - size / 2}px`;
-    newDiv.style.top = `${posY - size / 2}px`;
-    newDiv.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    const circleDiv = document.createElement('div');
+    circleDiv.classList.add('circle');
+    circleDiv.style.width = `${size}px`;
+    circleDiv.style.height = `${size}px`;
+    circleDiv.style.left = `${posX - size / 2}px`;
+    circleDiv.style.top = `${posY - size / 2}px`;
+    circleDiv.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 
     // Ajouter le div dans la page
-    document.body.appendChild(newDiv);
+    document.body.appendChild(circleDiv);
 
     // Selectionne le dernier cercle et le fait disparaitre une fois le bas de window atteint
-    const lastDiv = document.querySelector('div:last-child');
     const pageHeight = window.innerHeight;
     setTimeout(() => {
-        lastDiv.style.top = `${pageHeight - size}px`;
+        circleDiv.style.top = `${pageHeight - size}px`;
     }, 10);
 
-    lastDiv.addEventListener('transitionend', function () {
-        lastDiv.style.opacity = '0';
+    circleDiv.addEventListener('transitionend', function () {
+        circleDiv.style.opacity = '0';
         setTimeout(() => {
-            lastDiv.remove();
+            circleDiv.remove();
         }, 1000);
     });
 });
