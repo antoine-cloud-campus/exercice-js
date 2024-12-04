f20a9ca745cd74cc2641c90189a929904ae23b32
Nubunduram <ceantoine@hotmail.fr>
2024-12-04 14:19:51 +0100

    feat: Selection de planète

--- Dec/mercredi/script.js
+++ Dec/mercredi/script.js
@@ -1,5 +1,7 @@
-const baseUrl = 'https://swapi.dev/api/';
 const currentPage = window.location.pathname;
+
+const baseUrl = 'https://swapi.dev/api/';
+const pathOptions = ['people', 'planets', 'films', 'species', 'vehicles', 'startships'];
 
 // Index HTML
 
@@ -13,6 +15,19 @@ function getInfos(option) {
 
 // Planets HTML
 
+function getPlanet(planetUrl) {
+
+    fetch(planetUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("planet-population").textContent = data.population;
+            document.getElementById("planet-diameter").textContent = data.diameter;
+            document.getElementById("planet-climate").textContent = data.climate;
+            document.getElementById("planet-gravity").textContent = data.population;
+            document.getElementById("planet-terrain").textContent = data.terrain;
+        })
+}
+
 function getPlanets() {
 
     fetch(baseUrl + 'planets')
@@ -21,12 +36,23 @@ function getPlanets() {
             document.getElementById("total-planet").textContent = data.count;
             const totalPlanet = data.count;
             const totalPage = data.count / data.results.length;
-            for (let i = 1; i < totalPage; i++) {
+            for (let i = 1; i <= totalPage; i++) {
                 fetch(baseUrl + 'planets/?page=' + i)
                     .then(response => response.json())
                     .then(data => {
                         data.results.forEach(planet => {
-                            document.getElementById("planet-list").innerHTML += `<li><p>${planet.name}</p><p>${planet.terrain}</p></li>`;
+                            const li = document.createElement("li");
+                            li.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
+
+                            li.addEventListener("click", () => {
+                                const defaultDiv = document.querySelector('.default-planet');
+                                const selectedDiv = document.querySelector('.selected-planet');
+                                defaultDiv.style.display = 'none';
+                                selectedDiv.style.display = 'flex';
+                                getPlanet(planet.url)
+                            });
+
+                            document.getElementById("planet-list").appendChild(li);
                         });
                     });
             }
