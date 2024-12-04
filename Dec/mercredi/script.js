d28e42d7d3f24567cf59799bfbe322d117bfe57f
Nubunduram <ceantoine@hotmail.fr>
2024-12-04 12:22:19 +0100

    feat: Planets Page

--- Dec/mercredi/script.js
+++ Dec/mercredi/script.js
@@ -1,4 +1,7 @@
-const baseUrl = 'https://swapi.dev/api/'
+const baseUrl = 'https://swapi.dev/api/';
+const currentPage = window.location.pathname;
+
+// Index HTML
 
 function getInfos(option) {
     fetch(baseUrl + option)
@@ -8,6 +11,35 @@ function getInfos(option) {
         });
 }
 
-getInfos('people');
-getInfos('planets');
-getInfos('vehicles');
+// Planets HTML
+
+function getPlanets() {
+
+    fetch(baseUrl + 'planets')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("total-planet").textContent = data.count;
+            const totalPlanet = data.count;
+            const totalPage = data.count / data.results.length;
+            for (let i = 1; i < totalPage; i++) {
+                fetch(baseUrl + 'planets/?page=' + i)
+                    .then(response => response.json())
+                    .then(data => {
+                        data.results.forEach(planet => {
+                            document.getElementById("planet-list").innerHTML += `<li><p>${planet.name}</p><p>${planet.terrain}</p></li>`;
+                        });
+                    });
+            }
+        })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (currentPage.endsWith("index.html")) {
+        getInfos('people');
+        getInfos('planets');
+        getInfos('vehicles');
+    } else if (currentPage.endsWith("planets.html")) {
+        getPlanets();
+    }
+});
+
