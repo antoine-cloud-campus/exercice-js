4e01c5e3376bfc4329e9f228993b6d54be95f834
Nubunduram <ceantoine@hotmail.fr>
2024-12-05 11:02:36 +0100

    feat: Filtering planets V1

--- Dec/mercredi/script.js
+++ Dec/mercredi/script.js
@@ -5,6 +5,12 @@ const baseUrl = 'https://swapi.dev/api/';
 // Index HTML
 const infosRequired = ['people', 'vehicles', 'planets']
 
+const defaultList = document.getElementById("planet-list");
+const filteredList = document.getElementById("planet-list-filtered");
+const selectElement = document.getElementById("planet-filter");
+const defaultDiv = document.querySelector('.default-planet');
+const selectedDiv = document.querySelector('.selected-planet');
+
 function getInfos(option) {
     fetch(baseUrl + option)
         .then(response => response.json())
@@ -28,12 +34,9 @@ function getPlanet(planetUrl) {
         })
 }
 
-async function createPlanetElement(data) {
+function updatePlanetFilteredList(data) {
 
-    const defaultDiv = document.querySelector('.default-planet');
-    const selectedDiv = document.querySelector('.selected-planet');
-
-    data.results.forEach(planet => {
+    data.forEach(planet => {
         const li = document.createElement("li");
         li.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
 
@@ -43,24 +46,67 @@ async function createPlanetElement(data) {
             selectedDiv.style.display = 'flex';
         });
 
-        document.getElementById("planet-list").appendChild(li);
+        filteredList.appendChild(li);
     });
 }
 
-async function getPlanets(url) {
+async function createPlanetElement(data, filter) {
+
+    let planetArray = data.results;
+
+    if (filter == '1') {
+        planetArray = planetArray.filter((planet) => parseInt(planet.population) <= 100000);
+        updatePlanetFilteredList(planetArray)
+    } else if (filter == '2') {
+        planetArray = planetArray.filter((planet) => parseInt(planet.population) > 100000 && parseInt(planet.population) <= 100000000);
+        updatePlanetFilteredList(planetArray)
+    } else if (filter == '3') {
+        planetArray = planetArray.filter((planet) => parseInt(planet.population) > 100000000);
+        updatePlanetFilteredList(planetArray)
+    } else {
+        planetArray.forEach(planet => {
+            const li = document.createElement("li");
+            li.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
+
+            li.addEventListener("click", () => {
+                defaultDiv.style.display = 'none';
+                getPlanet(planet.url);
+                selectedDiv.style.display = 'flex';
+            });
+
+            defaultList.appendChild(li);
+        });
+    }
+}
+
+async function getPlanets(url, filter) {
     try {
         while (url) {
             const response = await fetch(url);
             const data = await response.json();
 
             document.getElementById("total-planet").textContent = data.count;
-            createPlanetElement(data);
+            createPlanetElement(data, filter);
             url = data.next;
         }
     } catch (error) {
         console.error("Error fetching planets:", error);
     }
 }
+
+// Filters Planet
+
+selectElement.addEventListener('change', (e) => {
+    if (!e.target.value) {
+        filteredList.style.display = "none"
+        defaultList.style.display = "grid"
+    } else {
+        filteredList.innerHTML = '<ul class="grid" id="planet-list-filtered"></ul>';
+        filteredList.style.display = "grid"
+        defaultList.style.display = "none"
+    }
+    getPlanets(baseUrl + 'planets', e.target.value)
+});
 
 // A l'initialisation de la page
 
