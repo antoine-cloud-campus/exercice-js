208636a2e549318f8cc819af04d3f772ab957ce3
Nubunduram <ceantoine@hotmail.fr>
2024-12-05 10:21:47 +0100

    REFACTO: getPlanets

--- Dec/mercredi/script.js
+++ Dec/mercredi/script.js
@@ -2,7 +2,8 @@ const currentPage = window.location.pathname;
 
 const baseUrl = 'https://swapi.dev/api/';
 
-// Get Infos pour Index HTML
+// Index HTML
+const infosRequired = ['people', 'vehicles', 'planets']
 
 function getInfos(option) {
     fetch(baseUrl + option)
@@ -27,39 +28,38 @@ function getPlanet(planetUrl) {
         })
 }
 
-// Désolé ici c'est la merde je vais clean plus tard ptetre !
-function getPlanets() {
+async function createPlanetElement(data) {
 
-    fetch(baseUrl + 'planets')
-        .then(response => response.json())
-        .then(data => {
+    const defaultDiv = document.querySelector('.default-planet');
+    const selectedDiv = document.querySelector('.selected-planet');
+
+    data.results.forEach(planet => {
+        const li = document.createElement("li");
+        li.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
+
+        li.addEventListener("click", () => {
+            defaultDiv.style.display = 'none';
+            getPlanet(planet.url);
+            selectedDiv.style.display = 'flex';
+        });
+
+        document.getElementById("planet-list").appendChild(li);
+    });
+}
+
+async function getPlanets(url) {
+    try {
+        while (url) {
+            const response = await fetch(url);
+            const data = await response.json();
+
             document.getElementById("total-planet").textContent = data.count;
-            const totalPlanet = data.count;
-            const totalPage = data.count / data.results.length;
-
-            for (let i = 1; i <= totalPage; i++) {
-                fetch(baseUrl + 'planets/?page=' + i)
-                    .then(response => response.json())
-                    .then(data => {
-                        data.results.forEach(planet => {
-
-                            const li = document.createElement("li");
-
-                            li.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
-
-                            li.addEventListener("click", () => {
-                                const defaultDiv = document.querySelector('.default-planet');
-                                const selectedDiv = document.querySelector('.selected-planet');
-                                defaultDiv.style.display = 'none';
-                                getPlanet(planet.url)
-                                selectedDiv.style.display = 'flex';
-                            });
-
-                            document.getElementById("planet-list").appendChild(li);
-                        });
-                    });
-            }
-        })
+            createPlanetElement(data);
+            url = data.next;
+        }
+    } catch (error) {
+        console.error("Error fetching planets:", error);
+    }
 }
 
 // A l'initialisation de la page
@@ -67,11 +67,11 @@ function getPlanets() {
 document.addEventListener('DOMContentLoaded', () => {
 
     if (currentPage.endsWith("index.html")) {
-        getInfos('people');
-        getInfos('planets');
-        getInfos('vehicles');
+        infosRequired.forEach(info => {
+            getInfos(info)
+        });
     } else if (currentPage.endsWith("planets.html")) {
-        getPlanets();
+        getPlanets(baseUrl + 'planets');
     }
 });
 
